6f6fa2c8b11368f50ecc94d222f75ee5a661d947
wanglu704 <57435476+wanglu704@users.noreply.github.com>
2023-04-10 21:14:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -74,26 +74,16 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'odCqD6mrqCKG0_uCP3_0n9YNN7Fw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7Fv5TR1wAarn5XypLWOp2r2VLloQ5WOIs9Eg3P8itmY',
+      useTemplateId: 'mE3HAieoehIG6YwHt_m-iS4Vv73P6Xl-lnPhDF50UB8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       province: '广东',
       // 所在城市或县区
       city: '广州',
-      province1: '北京',
-      // 所在城市或县区
-      city1: '北京',
       horoscopeDate: '12-09',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '璐璐', year: '1992', date: '11-16',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '璐璐', year: '1992', date: '12-09',
-        },
-        {
-          type: '*生日', name: '飞飞', year: '2001', date: '03-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
@@ -101,12 +91,17 @@ const USER_CONFIG = {
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '11-21',
+        },
+        {
+          type: '节日', name: '相恋纪念日', year: '2023', date: '01-01',
         }
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-01-01' },
+        // 在一起的日子
+        { keyword: 'meet_day', date: '2022-11-21' },
         // 在一起的日子
         { keyword: 'lulu_day', date: '1992-12-09' },
         // 退伍日
